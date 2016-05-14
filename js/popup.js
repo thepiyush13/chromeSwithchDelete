54b5f6be400cd74ba2317dd91da78dc3e56418b1
piyush <thepiyush13@gmail.com>
2016-05-13 19:17:12 -0500

    v2.0 with UI updates and keyboard support

--- js/popup.js
+++ js/popup.js
@@ -1,61 +1,46 @@
+
+// References : http://evulse.github.io/finderSelect/
+
+
+
+//global tablist object
+var gTabListId = "tabList";  //for tablist html ID
+var gTabList = "";  //for tablist object
+var gDelBtnId = "delSelected";
+var gSelectBtnId = "selectToggle";
+
+
 function renderStatus(statusText) {
 	document.getElementById('status').innerText = statusText;
 }
 
-
-//event listner for shortcut commands up and down key
-chrome.commands.onCommand.addListener(function(command) {
-        console.log('Command:', command);
-});
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
 
 //add event listner for selectall checkbox
-init();
+
 
 document.addEventListener("click", function(e){ 
-	console.log('click');
+	
 	var srcElement = e.srcElement;
 	var cid = (this.activeElement.id);
+
   // Lets check if our underlying element is a LINK.
-  if (srcElement.nodeName == 'A') {
-  	var href = srcElement.href;    
-  	var temp = cid.split("|")
-  	var windowId = temp[0]
-  	var id = temp[1]
+	if (srcElement.nodeName == 'A') {		
+		handleLinkClick(srcElement,cid);
+	}
+	//check if deleteSelected button is clicked
+	else if(cid==gDelBtnId){
+		handleDeleteEvent();	  		
 
-  	chrome.runtime.getBackgroundPage(function(eventPage) {
-  		eventPage.switchTab(windowId,id);
-  	});
-  }
-	  	//check if deleteSelected button is clicked
-	  	else if(cid=="delSelected"){
-	  		var tabIds = getSelectedTabs();
-			//remove tab checkboxes from front end
-	  		//removeTabs(tabIds);
-	  		//remove actual tabs 
-	  		document.getElementById('status').innerHTML = 
-	  		"<br/><h1>Deleting "+tabIds.length +" tabs...</h1>";
-	  		chrome.runtime.getBackgroundPage(function(eventPage) {
-	  			eventPage.deleteTabs(tabIds);
-	  		});
-			// showTabListing();
-
-	  	}
-	  
-	  else{
-	  	//show tab list 
-
-	  }
+	}else if(cid==gSelectBtnId){
+	//check if selectAll button is clicked
+		handleSelectEvent(this.activeElement);			
+	}	
 
 
 	});
 
-		//show tab list 
+//show tab list 
 showTabListing();
 
 });
@@ -63,45 +48,38 @@ showTabListing();
 
 //function get tab listings in html list
 function showTabListing(){
-	var tabList = "<ul id='tabList'>";
+	var tabList = "<ul class='list-group' id='"+gTabListId+"'>";
+	var tempFocus = 0;
  chrome.tabs.query({}, function(tabs) { // blah 
  	for(var i=0;i<tabs.length;i++){
  		var title = tabs[i].title.substring(0, 70)+".." //only length x title is added
- 		tabList+="<li><input type='checkbox' name='tabCheckBox' value='"+tabs[i].id+"'>&nbsp;<a id='"+tabs[i].windowId+"|"+tabs[i].id+	"' href='"+tabs[i].url+"'>";
- 		tabList+="<img src='"+tabs[i].favIconUrl+"' style='width:16px;height:16px;'>&nbsp;";
- 		tabList+=title;
- 		tabList+="</a></li>";
+ 		tabList+="<li class='list-group-item un-selected' id='"+tabs[i].id+"'>";
+ 		tabList+="<img src='"+tabs[i].favIconUrl+"' class='tabFavicon'>&nbsp;";
+ 		tabList+="<span>"+title+"</span>";	
+ 		tabList+="<a class='pull-right' id='"+tabs[i].windowId+"|"+tabs[i].id+	"' href='"+tabs[i].url+"'>Open</a></li>"; 			 
+		tempFocus++;
  	}
  	tabList+="</ul>"
 
- 	document.getElementById('status').innerHTML = tabList;
+ 	document.getElementById('tabContents').innerHTML = tabList;
+ 	//apply cosmetic improvements for user accessibility
+ 	finalize();
 
  });
 }
-//toggles the popup tablist checkbox for select all etc
-function init(){
-	console.log('init');
-	var checkbox = document.getElementById("selectAll");
-	if (checkbox.addEventListener) {
-		checkbox.addEventListener ("change", OnChangeCheckbox, false);
-	}
+//init for tablist and search box
+function finalize(){
+		gTabList = $('#'+gTabListId).finderSelect({totalSelector:".tab-count",menuSelector:"#tab-menu",selectClass:'active'});
+	 $('#'+gTabListId).btsListFilter('#searchinput', {itemChild: 'span'});
+	
 }
 
-function OnChangeCheckbox (event) {
-	console.log('checked');
-	var checkbox = event.target;
-	if (checkbox.checked) {
-		updateAllTabs(true);
-
-	}
-	else {
-		updateAllTabs(false);
-	}
-}
+//select all and select None tabs
 function updateAllTabs(status) {
-	var checkboxes = document.getElementsByName('tabCheckBox');
-	for(var i=0, n=checkboxes.length;i<n;i++) {
-		checkboxes[i].checked = status;
+	if(status){
+		gTabList.finderSelect('highlightAll');
+	}else{
+		gTabList.finderSelect('unHighlightAll');
 	}
 }
 //removes tab checkboxes listing from popup html
@@ -118,20 +96,77 @@ function removeTabs(tabIds){
 //returns tabids for selected tabs 
 function getSelectedTabs(){
 
-	var checkboxes = document.getElementsByName('tabCheckBox');
+	console.log(gTabList.finderSelect("selected")	);
+	var selectedTabs = gTabList.finderSelect("selected");
 	var tabIds = [];
-	for(var i=0, n=checkboxes.length;i<n;i++) {
-		if(checkboxes[i].checked == true){
-			tabIds.push(parseInt(checkboxes[i].value));
-		}
+	for(var i=0, n=selectedTabs.length;i<n;i++) {
+		tabIds.push(parseInt(selectedTabs[i].id));
 	}
 	return tabIds;
 }
 
 
-//listen to background orders
+//listen to background orders after processing
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if(request.action=='update'){
-		showTabListing();
+		gDelBtn.button('reset'); //reset delete button text
+		setTimeout(function(){showTabListing();}, 1000);
+		return;
+		//showTabListing();
 	}
 });
+
+// providing some additional keyboard functions
+window.addEventListener("keydown", checkKeyPressed, false);
+ 
+function checkKeyPressed(e) {
+    if (e.keyCode == "13") { //enter key
+        var selected = gTabList.finderSelect('selected');
+        //if only one element is selected then switch else do nothing                
+        if(selected.length==1){
+        	var link  = selected.children('A')[0];
+        	var cid = link.id;
+        	handleLinkClick(selected,cid);
+        }
+    }else if(e.keyCode=="46"){ //delete key
+    	handleDeleteEvent();
+    }
+}
+
+//handle the delete button click event
+function handleDeleteEvent(){
+	var tabIds = getSelectedTabs();
+			//remove tab checkboxes from front end
+	  		//removeTabs(tabIds);
+	  		//remove actual tabs 
+	  		gDelBtn = $('#'+gDelBtnId).button('loading')
+
+	  		chrome.runtime.getBackgroundPage(function(eventPage) {
+	  			eventPage.deleteTabs(tabIds);
+	  		});
+			// showTabListing();
+}
+
+//handles selection toggle button click event
+function handleSelectEvent(e){
+			if(e.value=="Select All"){
+				updateAllTabs(true);
+				e.value="Select None";
+			}else{
+				updateAllTabs(false);
+				e.value="Select All";
+			}
+			return;
+}
+
+//handles the tab link click events
+function handleLinkClick(srcElement,cid){		
+		var href = srcElement.href;    
+		var temp = cid.split("|")
+		var windowId = temp[0]
+		var id = temp[1]
+
+		chrome.runtime.getBackgroundPage(function(eventPage) {
+			eventPage.switchTab(windowId,id);
+		});
+}
